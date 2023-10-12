046f40bff2f054f67a291f31a526bfd7bdfc9756
Razorua20 <simon.delafuente20@gmail.com>
2023-10-12 02:01:29 -0300

    creacion del modulo de seleccionar area

--- backend/bd/conexionbd.js
+++ backend/bd/conexionbd.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'bd_capstone',
+    database: 'capstone_bd',
 });
 
 db.connect((err) => {
