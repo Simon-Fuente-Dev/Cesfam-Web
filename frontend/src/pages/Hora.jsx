a295d9a6274d7714842ecd86396bccbbad933ae3
Razorua20 <simon.delafuente20@gmail.com>
2023-10-13 16:49:26 -0300

    hice el useState

--- frontend/src/pages/Hora.jsx
+++ frontend/src/pages/Hora.jsx
@@ -1,11 +1,16 @@
-
+import { useState } from "react";
+import swal from "sweetalert"
 const Hora = () => {
+  const [especialista, setEspecialista] = useState('');
+  const [fecha, setFecha] = useState('');
   return (
     <section-hora>
       <div className="agendar">
         <div className="especialista">
           <h2>Seleccione un especialista con el que desee atenderse</h2>
-          <select>
+          <select 
+            value={especialista}
+            onChange= {(e) => setEspecialista(e.target.value)}>
             <option value="">Seleccione un Especialista..</option>
             <option value="Ricardo">Ricardo Perez</option>
             <option value="Juan">Juan Carlos Bodoque</option>
@@ -13,7 +18,9 @@ const Hora = () => {
         </div>
         <div className="fecha">
           <h2>Elija una fecha para su hora</h2>
-          <select>
+          <select
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}>
             <option value="">Seleccione una fecha..</option>
             <option value="Ricardo">15/12/2023</option>
             <option value="Juan">16/12/2023</option>
